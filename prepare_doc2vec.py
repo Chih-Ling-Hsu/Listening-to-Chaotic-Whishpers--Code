9a3a12a6e8a9a7971dfbee24e3f5be94085efcc9
AmyHsu <oranse0720@gmail.com>
2019-07-15 15:22:22 +0800

    modification for bugs

--- prepare_doc2vec.py
+++ prepare_doc2vec.py
@@ -16,7 +16,7 @@ def prepare_data(ROOT, companyList):
     return [TaggedDocument(words=word_tokenize(str(_d.lower())), tags=[_t]) for _d, _t in zip(data, tag)]
 
 
-def train_doc2vec_model(ROOT, tagged_data, save_path, max_epochs=15, vec_size=200, alpha=0.025):
+def train_doc2vec_model(tagged_data, save_path, max_epochs=15, vec_size=200, alpha=0.025):
     model = Doc2Vec(vector_size=vec_size, alpha=alpha, min_alpha=0.025, min_count=5, dm=1, workers=16)
     model.build_vocab(tagged_data)
     
@@ -31,5 +31,5 @@ def train_doc2vec_model(ROOT, tagged_data, save_path, max_epochs=15, vec_size=20
     model.save(save_path)
 
 if __name__ == '__main__':
-    tagged_data = prepare_data(companyList)
+    tagged_data = prepare_data(ROOT, companyList)
     train_doc2vec_model(tagged_data, '{}/saved/d2v.model'.format(ROOT), max_epochs=15, vec_size=200, alpha=0.025)
